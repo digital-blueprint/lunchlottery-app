e0f894bc3bb6700c6b275c1742d0a35babcf488e
Patrizio Bekerle <patrizio@bekerle.com>
2024-01-24 14:59:47 +0100

    add: missing dbp-translated and webcomponents/scoped-custom-element-registry

--- src/dbp-lunchlottery-app.js
+++ src/dbp-lunchlottery-app.js
@@ -1,6 +1,9 @@
+import '@webcomponents/scoped-custom-element-registry';
 import {AppShell} from '@dbp-toolkit/app-shell';
 import * as commonUtils from '@dbp-toolkit/common/utils';
+import {Translated} from '@dbp-toolkit/common/src/translated';
 import {Logo} from '@tugraz/web-components';
 
 commonUtils.defineCustomElement('dbp-lunchlottery-app', AppShell);
+commonUtils.defineCustomElement('dbp-translated', Translated);
 commonUtils.defineCustomElement('tug-logo', Logo);
