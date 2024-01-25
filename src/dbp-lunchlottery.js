617adcbb309372531180ec561c95e2b2c7a353f3
Patrizio Bekerle <patrizio@bekerle.com>
2024-01-25 14:28:27 +0100

    do: organization fetching and HTML form handling

--- src/dbp-lunchlottery.js
+++ src/dbp-lunchlottery.js
@@ -19,9 +19,10 @@ class StarterActivity extends ScopedElementsMixin(DBPLitElement) {
         this.firstName = null;
         this.lastName = null;
         this.email = null;
-        this.organizationIds = null;
-        this.organization = null;
+        this.organizationId = null;
+        this.organizationName = null;
         this.entryPointUrl = null;
+        this.language = null;
     }
 
     static get scopedElements() {
@@ -39,12 +40,12 @@ class StarterActivity extends ScopedElementsMixin(DBPLitElement) {
             firstName: {type: String},
             lastName: {type: String},
             email: {type: String},
-            organizationIds: {type: Array},
-            organization: {type: String},
+            organizationId: {type: Number},
+            organizationName: {type: String},
             entryPointUrl: {type: String, attribute: 'entry-point-url'},
         };
     }
-    async autoFill() {
+    async fetchPerson() {
         let response = await fetch(this.entryPointUrl + '/base/people/' + this.auth['user-id'] + '?includeLocal=email,staffAt', {
             headers: {
                 'Content-Type': 'application/ld+json',
@@ -58,47 +59,39 @@ class StarterActivity extends ScopedElementsMixin(DBPLitElement) {
 
         const data = await response.json();
 
-        this.firstName = data['givenName'];
-        this.lastName = data['familyName'];
-        this.email = data['localData']['email'];
-        //this.organizationIds = data['localData']['staffAt'];
-
-        console.log(data);
+        this.firstName = data.givenName;
+        this.lastName = data.familyName;
+        this.email = data.localData.email ?? '';
+        this.organizationId = data.localData.staffAt[0] ?? null;
     }
 
     connectedCallback() {
         super.connectedCallback();
     }
 
-    async getOrganizations() {
-        let response = await fetch(this.entryPointUrl + '/base/organizations/' + this.organizationIds[0], {
+    async fetchOrganization() {
+        if (!this.organizationId) {
+            this.organizationName = '';
+            return;
+        }
+
+        let response = await fetch(this.entryPointUrl + '/base/organizations/' + this.organizationId, {
             headers: {
                 'Content-Type': 'application/ld+json',
                 Authorization: 'Bearer ' + this.auth.token,
             },
         });
+
         if (!response.ok) {
             throw new Error(response);
         }
 
         let data = await response.json();
-        console.log(data);
-
-        const organization = this._('#organization');
-
-        this.organization = `${data['name']}`;
-
-        organization.value = this.organization;
-
-            //this.organizations.push(data['name']);
-
-        //console.log(this.organizations);
-        //console.log(data);
+        this.organizationName = data.name ?? '';
     }
 
 
     update(changedProperties) {
-
         changedProperties.forEach((oldValue, propName) => {
             switch (propName) {
                 case 'lang':
@@ -106,8 +99,11 @@ class StarterActivity extends ScopedElementsMixin(DBPLitElement) {
                     break;
                 case 'auth':
                     if (this.auth && this.auth['login-status'] === 'logged-in') {
-                        this.autoFill();
+                        this.fetchPerson();
                     }
+                    break;
+                case 'organizationId':
+                    this.fetchOrganization();
                     break;
             }
         });
@@ -122,27 +118,29 @@ class StarterActivity extends ScopedElementsMixin(DBPLitElement) {
                 .hidden {
                     display: none;
                 }
-                
+
                 a {
                     color: blue;
                 }
-                
+
                 textarea {
                     width: 100%;
                     resize: none;
                 }
-                
+
                 .textField{
                     width: 100%;
                 }
-                
+
                 #rightSide {
                 float:right;
             `,
         ];
     }
 
-
+    languageClick(e) {
+        this.language = e.target.value;
+    }
 
     render() {
         let loggedIn = this.auth && this.auth.token;
@@ -154,14 +152,12 @@ class StarterActivity extends ScopedElementsMixin(DBPLitElement) {
             <p>${this.activity.getDescription(this.lang)} <a href="https://tu4u.tugraz.at/go/lunch-lottery">${this.activity.getHere(this.lang)}</a></p>
             <!--<div id="person-info"></div>-->
             <div class="${loggedIn ? '' : 'hidden'}">
-                
                 <div class="field">
                     <label class="label">${i18n.t('name.first')}</label>
                     <div class="control">
                         <input type="text" class="textField" value="${this.firstName}" readonly/>
                     </div>
                 </div>
-                
 
                 <div class="field">
                     <label class="label">${i18n.t('name.last')}</label>
@@ -169,77 +165,92 @@ class StarterActivity extends ScopedElementsMixin(DBPLitElement) {
                         <input type="text" class="textField" value="${this.lastName}" readonly/>
                     </div>
                 </div>
-                
+
                 <div class="field">
                     <label class="label">${i18n.t('organization')}</label>
                     <div class="control">
-                        <input type="text" class="textField" readonly/>
-                    </div>    
+                        <input type="text" class="textField" value="${this.organizationName}" readonly/>
+                    </div>
                 </div>
 
-                
                 <div class="field">
                     <label class="label">${i18n.t('email')}</label>
                     <div class="control">
                         <input type="email" class="textField" value="${this.email}" readonly/>
                     </div>
-                    
                 </div>
-                
+
                 <div class="field">
                     <label class="label">${i18n.t('languages.label')}</label>
                     <div class="control">
-                        <input type="radio" id="german" name="language" value="german">
-                        <label for="german">${i18n.t('languages.german')}</label><br>
-                        <input type="radio" id="english" name="language" value="english">
-                        <label for="english">${i18n.t('languages.english')}</label><br>
-                        <input type="radio" id="both" name="language" value="both">
-                        <label for="both">${i18n.t('languages.both')}</label>
+                        <div>
+                            <input type="radio" id="language-german" name="language" value="german" @click="${this.languageClick}">
+                            <label for="language-german">${i18n.t('languages.german')}</label>
+                        </div>
+                        <div>
+                            <input type="radio" id="language-english" name="language" value="english" @click="${this.languageClick}">
+                            <label for="language-english">${i18n.t('languages.english')}</label>
+                        </div>
+                        <div>
+                            <input type="radio" id="language-both" name="language" value="both" @click="${this.languageClick}">
+                            <label for="language-both">${i18n.t('languages.both')}</label>
+                        </div>
                     </div>
-                    
+
                 </div>
                 <!-- Should I add the provided dates, or make a webcomponent that lets the customers choose the dates themselves? -->
                 <div class="field">
                     <label class="label">${i18n.t('day.label')}</label>
                     <div class="control">
-                        <input type="checkbox" id="wednesday" name="wednesday" value="wednesday">
-                        <label for="wednesday">${i18n.t('day.wednesday')}</label><br>
-                        <input type="checkbox" id="thursday" name="thursday" value="thursday">
-                        <label for="thursday">${i18n.t('day.thursday')}</label><br>
-                        <input type="checkbox" id="friday" name="friday" value="friday">
-                        <label for="friday">${i18n.t('day.friday')}</label><br>
-                        <input type="checkbox" id="monday" name="monday" value="monday">
-                        <label for="monday">${i18n.t('day.monday')}</label><br>
-                        <input type="checkbox" id="tuesday" name="tuesday" value="tuesday">
-                        <label for="tuesday">${i18n.t('day.tuesday')}</label>
+                        <div>
+                            <input type="checkbox" id="wednesday" name="wednesday" value="wednesday">
+                            <label for="wednesday">${i18n.t('day.wednesday')}</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="thursday" name="thursday" value="thursday">
+                            <label for="thursday">${i18n.t('day.thursday')}</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="friday" name="friday" value="friday">
+                            <label for="friday">${i18n.t('day.friday')}</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="monday" name="monday" value="monday">
+                            <label for="monday">${i18n.t('day.monday')}</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="tuesday" name="tuesday" value="tuesday">
+                            <label for="tuesday">${i18n.t('day.tuesday')}</label>
+                        </div>
                     </div>
                 </div>
-                
+
                 <div class="field">
                     <label class="label">${i18n.t('agreement.label')}</label>
                     <div class="control">
-                        <input type="radio" id="yes" name="yes" value="yes">
-                        <label for="yes">${i18n.t('agreement.yes')}</label>
+                        <div>
+                            <input type="radio" id="yes" name="yes" value="yes">
+                            <label for="yes">${i18n.t('agreement.yes')}</label>
+                        </div>
+                        <div>
                         <input type="radio" id="no" name="no" value="no">
-                        <label for="no">${i18n.t('agreement.no')}</label>
+                            <label for="no">${i18n.t('agreement.no')}</label>
+                        </div>
                     </div>
-                    
                 </div>
-                
+
                 <div id="rightSide">
                     <dbp-button 
                        value="Primary"
                        @click="${this.buttonClickHandler}"
                        type="is-primary">${i18n.t('submit')}</dbp-button>
                 </div>
-                
+
             </div>
 
             <div class="${!loggedIn ? '' : 'hidden'}">
                 <p>${i18n.t('please-log-in')}</p>
             </div>
-            
-            
         `;
     }
 }
