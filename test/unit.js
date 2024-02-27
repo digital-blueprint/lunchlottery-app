8163800a026b003bf892a4f4c547e0e6ad5f614e
Tobias Gross-Vogt <tobias.gross-vogt@tugraz.at>
2024-02-27 15:17:15 +0100

    fix test

--- test/unit.js
+++ test/unit.js
@@ -1,13 +1,13 @@
 import {assert} from 'chai';
 
-import '../src/dbp-lunchlottery';
+import '../src/dbp-lunchlottery-register';
 import '../src/dbp-lunchlottery-app.js';
 
 suite('dbp-lunchlottery basics', () => {
     let node;
 
     suiteSetup(async () => {
-        node = document.createElement('dbp-lunchlottery');
+        node = document.createElement('dbp-lunchlottery-register');
         document.body.appendChild(node);
         await node.updateComplete;
     });
