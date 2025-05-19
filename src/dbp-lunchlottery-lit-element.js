digital-blueprint/lunchlottery-app

commit 0990237448a5b6d450fdb0d81929dd6c17bed96f

No mre redirection to login

src/dbp-lunchlottery-lit-element.js
@@ -28,16 +28,7 @@ export default class DBPLunchlotteryLitElement extends DBPLitElement {
      */
     _updateAuth() {
         this._loginStatus = this.auth['login-status'];
-
-        if (this._loginStatus === LoginStatus.LOGGED_OUT) {
-            this.sendSetPropertyEvent('requested-login-status', LoginStatus.LOGGED_IN);
-        } else if (this.auth && this.auth['login-status'] === LoginStatus.LOGGED_IN) {
-            if (!this.initialized) {
-                this.initialize();
-                this.initialized = true;
-            }
-        }
-
+        // Every time isLoggedIn()/isLoading() return something different we request a re-render
         let newLoginState = [this.isLoggedIn(), this.isLoading()];
         if (this._loginState.toString() !== newLoginState.toString()) {
             this.requestUpdate();

src/dbp-lunchlottery-register.js
@@ -408,7 +408,7 @@ class LunchLotteryRegister extends ScopedElementsMixin(DBPLunchlotteryLitElement
         const showSuccessText = this.isLoggedIn() && this.wasSubmissionSuccessful;
 
         return html`
-            <h2>${this.activity.getName(this.lang)}</h2>
+            <!--<h2>${this.activity.getName(this.lang)}</h2>-->
             <p class="subheadline">
                 <slot name="description">
                     ${this.activity.getDescription(this.lang)}

commit b04c591933b925710df76281413334db301e742a

Restore form initialization
regression from 0990237448a5b6d450fdb0d81929dd6c17bed96f

src/dbp-lunchlottery-lit-element.js
@@ -1,4 +1,5 @@
 import DBPLitElement from '@dbp-toolkit/common/dbp-lit-element';
+import {LoginStatus} from "@dbp-toolkit/auth/src/util";
 import {send} from '@dbp-toolkit/common/notification';
 
 export default class DBPLunchlotteryLitElement extends DBPLitElement {
@@ -27,6 +28,14 @@ export default class DBPLunchlotteryLitElement extends DBPLitElement {
      */
     _updateAuth() {
         this._loginStatus = this.auth['login-status'];
+
+        if (this.auth && this.auth['login-status'] === LoginStatus.LOGGED_IN) {
+            if (!this.initialized) {
+                this.initialize();
+                this.initialized = true;
+            }
+        }
+
         // Every time isLoggedIn()/isLoading() return something different we request a re-render
         let newLoginState = [this.isLoggedIn(), this.isLoading()];
         if (this._loginState.toString() !== newLoginState.toString()) {