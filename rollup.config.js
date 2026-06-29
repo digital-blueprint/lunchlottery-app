30326dcbc96236f9a6e937041234fd4cfb8cf0c8
Christoph Reiter <reiter.christoph@gmail.com>
2026-06-29 13:15:24 +0200

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -318,10 +318,8 @@ export default (async () => {
                         [
                             '@babel/preset-env',
                             {
-                                loose: false,
                                 modules: false,
                                 shippedProposals: true,
-                                bugfixes: true,
                                 targets: {
                                     esmodules: true,
                                 },
